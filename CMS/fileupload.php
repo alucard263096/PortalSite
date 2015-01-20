1b8f3e3abd43ea8a34607d88f171eedb10dd1671
Steve Cai <steve.cai@morningstar.com>
2015-01-20 17:46:41 +0800

    commit cms

---
+++ CMS/fileupload.php
@@ -0,0 +1,24 @@
+<?php
+ require 'include/common.inc.php';
+ require ROOT.'/classes/obj/upload.php';
+ $field=$_REQUEST["field"];
+ $module=$_REQUEST["module"];
+ $file=$_FILES[$field];
+ if($file==""){
+	echo "Fils is empty";
+	exit;
+ }
+ $filename=date('ymdHIs').$file["name"];
+ $folder=ROOT."/".$CONFIG['fileupload']['upload_path']."/$module/";
+ if(!file_exists($folder)){
+	mkdir($folder,0777);
+ }
+ $file=new Upload($file,$filename,$folder,true);
+ echo $file->safetyUpload();
+ echo "|~~|".$filename;
+
+
+
+
+
+?>
